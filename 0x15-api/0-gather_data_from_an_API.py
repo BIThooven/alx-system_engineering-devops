36f81c4ca04070198d4ce8230308fa441915cfec
BIThooven <yassine.parkourist@gmail.com>
2024-03-25 21:58:46 +0000

    gathering infos from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""requesting data from an api"""
+import requests
+import re
+import sys
+
+
+API_url = "https://jsonplaceholder.typicode.com"
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(API_url, id)).json()
+            task_req = requests.get('{}/todos'.format(API_url)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
