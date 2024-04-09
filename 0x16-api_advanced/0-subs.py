2468c2e5eb46ffc6d56c2c8ae369baea0006f81c
BIThooven <yassine.parkourist@gmail.com>
2024-04-09 23:17:00 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user = {'User-agent': 'Google Chrome Version 123.0.6312.106'}
+    user = {'User-Agent': 'Google Chrome Version 123.0.6312.106'}
     Url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(Url, headers=user)
     res = response.json()
