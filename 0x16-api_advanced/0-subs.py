154a4c2caadaca1410bf419bcedc3810da8cc91f
BIThooven <yassine.parkourist@gmail.com>
2024-04-09 23:16:05 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,11 @@
 """number of subs for a subreddit"""
 from requests import get
 
+
 def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
+
     user = {'User-agent': 'Google Chrome Version 123.0.6312.106'}
     Url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(Url, headers=user)
